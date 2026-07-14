3936566ae2d87b56fd17d6618c50f21346d8ae81
sulcruinfo-dev <sulcru.info@gmail.com>
2026-07-14 14:11:36 +0200

    Update SulcruOrganic.jsx

--- sulcru-organic/src/SulcruOrganic.jsx
+++ sulcru-organic/src/SulcruOrganic.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { FaWhatsapp } from "react-icons/fa";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 
 export default function SulcruOrganic() {
   const location = useLocation();
@@ -609,19 +610,7 @@ const message = form.get('message');
     </div>
   </div>
 )}
-<a
-  href="https://wa.me/27615141042?text=Hello%20Sulcru%20Organic,%20I%20would%20like%20more%20information."
-  target="_blank"
-  rel="noopener noreferrer"
-className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-2xl transition-all duration-300 hover:scale-110 animate-pulse">
-<>
-  <FaWhatsapp size={32} />
-
-  <div className="absolute right-20 bg-black text-white px-3 py-2 rounded-lg text-sm whitespace-nowrap opacity-0 hover:opacity-100 transition">
-    Chat with us
-  </div>
-</>
-</a>
+<FloatingWhatsApp />
 </>
 ); 
 }

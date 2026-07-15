f7a2d365cfabd3a668c1577d1ad7becbd7b56447
sulcruinfo-dev <sulcru.info@gmail.com>
2026-07-15 16:23:02 +0200

    Update ProductLayout.jsx

--- sulcru-organic/src/layouts/ProductLayout.jsx
+++ sulcru-organic/src/layouts/ProductLayout.jsx
@@ -1,7 +1,19 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import FloatingWhatsApp from "../components/FloatingWhatsApp";
+
 export default function ProductLayout() {
   return (
-    <div>
-      Product Layout
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+      
+      <main className="min-h-screen">
+        Product Layout
+      </main>
+
+      <Footer />
+
+      <FloatingWhatsApp />
     </div>
   );
 }

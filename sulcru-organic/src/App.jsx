7e0afc4188bf6cc6f5fdb532399a2a72e90c2461
sulcruinfo-dev <sulcru.info@gmail.com>
2026-07-15 16:14:42 +0200

    Update App.jsx

--- sulcru-organic/src/App.jsx
+++ sulcru-organic/src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
       <Route path="/" element={<SulcruOrganic />} />
       <Route path="/about" element={<SulcruOrganic />} />
       <Route path="/products" element={<SulcruOrganic />} />
+      <Route path="/products/soil-improver" element={<SoilImproverPage />} />
       <Route path="/product" element={<SulcruOrganic />} />
       <Route path="/ingredients" element={<SulcruOrganic />} />
       <Route path="/benefits" element={<SulcruOrganic />} />

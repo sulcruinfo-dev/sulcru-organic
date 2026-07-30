7e99da10a65b539e17a51a768d6b6446876ba888
sulcruinfo-dev <sulcru.info@gmail.com>
2026-07-30 15:20:46 +0200

    Update Footer.jsx new email

--- sulcru-organic/src/components/Footer.jsx
+++ sulcru-organic/src/components/Footer.jsx
@@ -7,6 +7,31 @@ export default function Footer() {
         <a href='https://www.youtube.com/@SulcruOrganic' target='_blank' rel='noreferrer' className='hover:text-yellow-400'>YouTube</a>
         <a href='https://www.tiktok.com/@sulcruorganic' target='_blank' rel='noreferrer' className='hover:text-yellow-400'>TikTok</a>
       </div>
+      <div className="mt-6 text-zinc-400 space-y-1">
+
+  <p>
+    General Enquiries:
+    <br />
+    <a
+      href="mailto:info@sulcruorganic.co.za"
+      className="hover:text-yellow-400"
+    >
+      info@sulcruorganic.co.za
+    </a>
+  </p>
+
+  <p>
+    Biotechnology & Research:
+    <br />
+    <a
+      href="mailto:biotech@sulcruorganic.co.za"
+      className="hover:text-yellow-400"
+    >
+      biotech@sulcruorganic.co.za
+    </a>
+  </p>
+
+</div>
       Sulcru Organic © 2026 • Belfast, South Africa
     </footer>
   );
